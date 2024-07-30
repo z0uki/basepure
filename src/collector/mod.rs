ebdbaa10f018c60cd7fce0d788a7f8c21f04f712
z0uki <3070700787@qq.com>
2024-07-30 16:10:52 +0800

    features

--- src/collector/mod.rs
+++ src/collector/mod.rs
@@ -1,15 +1,27 @@
+#[cfg(feature = "eth")]
 mod block_collector;
+#[cfg(feature = "eth")]
 mod full_block_collector;
-mod interval_collector;
+#[cfg(feature = "eth")]
 mod log_collector;
+#[cfg(feature = "eth")]
 mod logs_in_block_collector;
+#[cfg(feature = "eth")]
 mod mempool_collector;
+#[cfg(feature = "eth")]
 mod poll_full_block_collector;
-
+#[cfg(feature = "eth")]
 pub use block_collector::BlockCollector;
+#[cfg(feature = "eth")]
 pub use full_block_collector::FullBlockCollector;
-pub use interval_collector::IntervalCollector;
+#[cfg(feature = "eth")]
 pub use log_collector::LogCollector;
+#[cfg(feature = "eth")]
 pub use logs_in_block_collector::LogsInBlockCollector;
+#[cfg(feature = "eth")]
 pub use mempool_collector::MempoolCollector;
+#[cfg(feature = "eth")]
 pub use poll_full_block_collector::PollFullBlockCollector;
+
+mod interval_collector;
+pub use interval_collector::IntervalCollector;
